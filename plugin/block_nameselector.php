05cef4645a4a06171e711a5f69592479970f9b35
Milan Donhowe <milanaugust+GIT@gmail.com>
2019-03-18 09:05:47 -0700

    changed string names
    changed boilerplate name to thing

--- plugin/block_nameselector.php
+++ plugin/block_nameselector.php
@@ -1,7 +1,7 @@
 <?php
-class block_simplehtml extends block_base {
+class block_nameselector extends block_base {
     public function init() {
-        $this->title = get_string('simplehtml', 'block_simplehtml');
+        $this->title = get_string('nameselector', 'block_nameselector');
     }
     // The PHP tag and the curly bracket for the class definition 
     // will only be closed after there is another function added in the next section.
--- plugin/lang/en/block_nameselector.php
+++ plugin/lang/en/block_nameselector.php
@@ -1,5 +1,5 @@
 <?php
-$string['pluginname'] = 'Simple HTML block';
-$string['simplehtml'] = 'Simple HTML';
+$string['pluginname'] = 'Name selector';
+$string['nameselector'] = 'Simple HTML';
 $string['simplehtml:addinstance'] = 'Add a new simple HTML block';
 $string['simplehtml:myaddinstance'] = 'Add a new simple HTML block to the My Moodle page';
